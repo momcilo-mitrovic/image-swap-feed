b3e1bcd5487f85d4470db6f2b240a562772f9941
Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>
2022-01-02 23:41:02 +0100

    I changed the way i did it, focused more on the js part
    Signed-off-by: Momcilo-Mitrovic <mitrovicmomcilo09@gmail.com>

--- image-feed/image.js
+++ image-feed/image.js
@@ -1,40 +1,28 @@
-const imgs = document.getElementById('imgs')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
-const img = document.querySelectorAll('#imgs img')
+var slike = [
+  'https://images.unsplash.com/photo-1640297013018-adaae8fd5536?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1640270219264-82bd8fb3c90a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1593642532009-6ba71e22f468?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+  'https://images.unsplash.com/photo-1638913662380-9799def8ffb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+]
+var slikeelements = null
+var container = null
+var index = 0
 
-let index = 0
-
-let interval = setInterval(run, 2000)
-
-function run() {
-  index++
-  Imagechanger()
-}
-
-function Imagechanger() {
-  if (index > img.length - 1) {
-    index = 0
-  } else if (index < 0) {
-    index = img.length - 1
+var buttons = document.getElementsByClassName('button')
+window.onload = function () {
+  console.log('test')
+  container = document.getElementById('imgs')
+  slikeelements = new Array(slike.length)
+  for (let i = 0; i < slike.length; i++) {
+    slikeelements[i] = document.createElement('img')
+    slikeelements[i].src = slike[i]
+    container.appendChild(slikeelements[i])
   }
-
-  imgs.style.transform = `translateX(${-index * 500}px)`
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+      index += buttons[i].id == 'left' ? -1 : 1
+      if (index < 0) index = slike.length - 1
+      container.style.transform = `translateX(${-index * 500}px)`
+    }
+  }
 }
-
-function resetInterval() {
-  clearInterval(interval)
-  interval = setInterval(run, 2000)
-}
-
-leftBtn.addEventListener('click', () => {
-  index--
-  Imagechanger()
-  resetInterval()
-})
-
-rightBtn.addEventListener('click', () => {
-  index++
-  Imagechanger()
-  resetInterval()
-})
